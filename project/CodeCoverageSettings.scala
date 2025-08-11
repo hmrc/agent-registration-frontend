e42964aa9bb5a9579eaa35bfcfa81ac3ec36d825
Valentin Lukovski <47025947+VLukovski@users.noreply.github.com>
2025-08-11 09:31:42 +0100

    APB-9732 GRS test coverage (#14)
    * APB-9732 GRS test coverage
    
    * readme
    
    * coverage fix

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -10,16 +10,13 @@ object CodeCoverageSettings {
     "app.*",
     "prod.*",
     ".*Routes.*",
-    "testOnly.*",
+    ".*testOnly.*",
     "testOnlyDoNotUseInAppConf.*",
-    "models.forms.testonly.*",
-    "controllers.testonly.*",
-    "views.html.testonly.*"
   )
 
   val settings: Seq[Setting[_]] = Seq(
-    ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 100,
+    ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(","),
+    ScoverageKeys.coverageMinimumStmtTotal := 90,
     ScoverageKeys.coverageFailOnMinimum := false,
     ScoverageKeys.coverageHighlighting := true
   )
