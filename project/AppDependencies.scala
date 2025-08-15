568b62f4e93858de1b62b6b5f2d2dedbbb5c94de
Valentin Lukovski <47025947+VLukovski@users.noreply.github.com>
2025-08-15 10:18:32 +0100

    pr bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.0.0"
+  private val bootstrapVersion = "10.1.0"
   private val hmrcMongoVersion = "2.7.0"
 
   val compile = Seq(
