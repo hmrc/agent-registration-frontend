7ac8d7b434878ce8390b3c76e862d155afd9f861
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-09-11 09:11:36 +0100

    APB-9751 dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,15 +8,15 @@ object AppDependencies {
   private val bootstrapVersion = "10.1.0"
   private val hmrcMongoVersion = "2.7.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.8.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.11.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % hmrcMongoVersion,
     "com.softwaremill.quicklens" %% "quicklens" % "1.9.12", // Updated for Scala 3 compatibility
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.3.0"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
     "org.jsoup" % "jsoup" % "1.21.2" % Test
