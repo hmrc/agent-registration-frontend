e73d7b8ab77fba2a9b9c178403ed1c951877926d
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-10-31 15:43:28 +0000

    dependencies and unignore

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,12 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.3.0"
+  private val bootstrapVersion = "10.4.0"
   private val hmrcMongoVersion = "2.10.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.18.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.20.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % hmrcMongoVersion,
     "com.softwaremill.quicklens" %% "quicklens" % "1.9.12", // Updated for Scala 3 compatibility
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.3.0"
