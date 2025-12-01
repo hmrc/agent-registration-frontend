d82011439377d1c779659deb6370dc8942fd2ea4
wojciech.gradzki <gitwojciech@users.noreply.github.com>
2025-12-01 13:16:26 +0000

    [WG][APB-10034] NINO - LLP, add page

---
+++ app/uk/gov/hmrc/agentregistrationfrontend/controllers/providedetails/MemberNinoController.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentregistrationfrontend.controllers.providedetails
+
+import com.softwaremill.quicklens.modify
+import play.api.mvc.Action
+import play.api.mvc.ActionBuilder
+import play.api.mvc.AnyContent
+import play.api.mvc.MessagesControllerComponents
+import uk.gov.hmrc.agentregistration.shared.llp.MemberIdentifiersSource.UserSupplied
+import uk.gov.hmrc.agentregistration.shared.llp.MemberProvidedDetails
+import uk.gov.hmrc.agentregistrationfrontend.action.Actions
+import uk.gov.hmrc.agentregistrationfrontend.action.providedetails.llp.MemberProvideDetailsRequest
+import uk.gov.hmrc.agentregistrationfrontend.controllers.FrontendController
+import uk.gov.hmrc.agentregistrationfrontend.forms.MemberNinoForm
+import uk.gov.hmrc.agentregistrationfrontend.services.llp.MemberProvideDetailsService
+import uk.gov.hmrc.agentregistrationfrontend.views.html.providedetails.memberconfirmation.MemberNinoPage
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import scala.concurrent.Future
+
+@Singleton
+class MemberNinoController @Inject() (
+  actions: Actions,
+  mcc: MessagesControllerComponents,
+  view: MemberNinoPage,
+  memberProvideDetailsService: MemberProvideDetailsService
+)
+extends FrontendController(mcc, actions):
+
+  private val baseAction: ActionBuilder[MemberProvideDetailsRequest, AnyContent] = actions.getProvideDetailsInProgress
+    .ensure(
+      mpd =>
+        mpd.memberProvidedDetails.ninoWithSource.isEmpty
+          || mpd.memberProvidedDetails.ninoWithSource.exists(_.source == UserSupplied),
+      implicit request =>
+        // TODO WG - next saUtr Page !!!
+        Redirect(routes.ExitController.genericExitPage)
+    )
+
+  def show: Action[AnyContent] = baseAction:
+    implicit request =>
+      Ok(view(
+        MemberNinoForm.form
+          .fill:
+            request
+              .memberProvidedDetails
+              .ninoWithSource
+      ))
+
+  def submit: Action[AnyContent] =
+    baseAction
+      .ensureValidFormAndRedirectIfSaveForLater(MemberNinoForm.form, implicit r => view(_))
+      .async:
+        implicit request: MemberProvideDetailsRequest[AnyContent] =>
+          MemberNinoForm.form
+            .bindFromRequest()
+            .fold(
+              formWithErrors => Future.successful(BadRequest(view(formWithErrors))),
+              ninoWithSource =>
+                val updatedApplication: MemberProvidedDetails = request
+                  .memberProvidedDetails
+                  .modify(_.ninoWithSource)
+                  .setTo(Some(ninoWithSource))
+                memberProvideDetailsService
+                  .upsert(updatedApplication)
+                  .map: _ =>
+                    Redirect(routes.ExitController.genericExitPage.url)
+            )
+      .redirectIfSaveForLater
