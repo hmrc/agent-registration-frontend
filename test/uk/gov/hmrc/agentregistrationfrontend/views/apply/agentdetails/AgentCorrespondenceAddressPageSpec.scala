5a9f3cceb0a9363a70adf3bc2975efd329d42aaa
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-12-04 14:05:53 +0000

    APB-10382 preserve alf address in cya change (#104)

--- test/uk/gov/hmrc/agentregistrationfrontend/views/apply/agentdetails/AgentCorrespondenceAddressPageSpec.scala
+++ test/uk/gov/hmrc/agentregistrationfrontend/views/apply/agentdetails/AgentCorrespondenceAddressPageSpec.scala
@@ -19,8 +19,10 @@ package uk.gov.hmrc.agentregistrationfrontend.views.apply.agentdetails
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import play.api.mvc.AnyContent
+import uk.gov.hmrc.agentregistration.shared.agentdetails.AgentCorrespondenceAddress
 import uk.gov.hmrc.agentregistrationfrontend.action.AgentApplicationRequest
 import uk.gov.hmrc.agentregistrationfrontend.forms.AgentCorrespondenceAddressForm
+import uk.gov.hmrc.agentregistrationfrontend.model.AddressOptions
 import uk.gov.hmrc.agentregistrationfrontend.model.SubmitAction.SaveAndComeBackLater
 import uk.gov.hmrc.agentregistrationfrontend.model.SubmitAction.SaveAndContinue
 import uk.gov.hmrc.agentregistrationfrontend.testsupport.ViewSpec
@@ -38,15 +40,27 @@ extends ViewSpec:
         .whenUsingExistingCompanyName
         .afterVerifiedEmailAddressSelected
   )
+  private val addressOptions = AddressOptions(
+    chroAddress = tdAll.companyProfile.unsanitisedCHROAddress,
+    bprAddress = Some(tdAll.bprRegisteredAddress),
+    otherAddress = None
+  )
+  private val addressOptionsWithOther = addressOptions.copy(
+    otherAddress = Some(AgentCorrespondenceAddress.fromAddressLookupAddress(tdAll.newCorrespondenceAddress))
+  )
   val doc: Document = Jsoup.parse(viewTemplate(
     form = AgentCorrespondenceAddressForm.form,
-    bprAddress = Some(tdAll.bprRegisteredAddress)
+    addressOptions = addressOptions
+  ).body)
+  val docWithOther: Document = Jsoup.parse(viewTemplate(
+    form = AgentCorrespondenceAddressForm.form,
+    addressOptions = addressOptionsWithOther
   ).body)
   private val heading: String = "What correspondence address should we use for your agent services account?"
 
   "AgentCorrespondenceAddressPage" should:
 
-    "contain expected content" in:
+    "contain expected content when there is no other address" in:
       doc.mainContent shouldContainContent
         """
           |Agent services account details
@@ -55,6 +69,24 @@ extends ViewSpec:
           |This is your Companies House registered office address.
           |Registered Line 1, Registered Line 2, AB1 2CD, GB
           |This is the address HMRC has in your business record.
+          |or
+          |Something else
+          |Save and continue
+          |Save and come back later
+          |Is this page not working properly? (opens in new tab)
+          |""".stripMargin
+
+    "contain expected content when there is an other address" in:
+      docWithOther.mainContent shouldContainContent
+        """
+          |Agent services account details
+          |What correspondence address should we use for your agent services account?
+          |23 Great Portland Street, London, W1 8LT, GB
+          |This is your Companies House registered office address.
+          |Registered Line 1, Registered Line 2, AB1 2CD, GB
+          |This is the address HMRC has in your business record.
+          |New Line 1, New Line 2, CD3 4EF, GB
+          |This is the address you have given us.
           |or
           |Something else
           |Save and continue
@@ -99,6 +131,6 @@ extends ViewSpec:
       behavesLikePageWithErrorHandling(
         field = field,
         errorMessage = errorMessage,
-        errorDoc = Jsoup.parse(viewTemplate(formWithError, Some(tdAll.bprRegisteredAddress)).body),
+        errorDoc = Jsoup.parse(viewTemplate(formWithError, addressOptions).body),
         heading = heading
       )
