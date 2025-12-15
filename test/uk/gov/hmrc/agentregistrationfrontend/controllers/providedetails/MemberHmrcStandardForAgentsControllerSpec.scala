fcdd4ddd510cc0e6c2353b48a388ea4309c98978
mrolfe88 <52818407+mrolfe88@users.noreply.github.com>
2025-12-15 12:58:58 +0000

    [APB-10308] Member LLP Agree standards page

--- test/uk/gov/hmrc/agentregistrationfrontend/controllers/providedetails/MemberHmrcStandardForAgentsControllerSpec.scala
+++ test/uk/gov/hmrc/agentregistrationfrontend/controllers/providedetails/MemberHmrcStandardForAgentsControllerSpec.scala
@@ -55,15 +55,14 @@ extends ControllerSpec:
     doc.title() shouldBe "Agree to meet the HMRC standard for agents - Apply for an agent services account - GOV.UK"
     doc.select("h2.govuk-caption-xl").text() shouldBe "HMRC standard for agents"
 
-  s"GET $path after agreeing terms should return 200 and render page" in:
+  s"GET $path after agreeing terms should redirect to the task list" in:
     AuthStubs.stubAuthoriseIndividual()
     AgentRegistrationMemberProvidedDetailsStubs.stubFindAllMemberProvidedDetails(List(memberProvidedDetails.afterTermsAgreed))
     val response: WSResponse = get(path)
 
-    response.status shouldBe Status.OK
-    val doc = response.parseBodyAsJsoupDocument
-    doc.title() shouldBe "Agree to meet the HMRC standard for agents - Apply for an agent services account - GOV.UK"
-    doc.select("h2.govuk-caption-xl").text() shouldBe "HMRC standard for agents"
+    response.status shouldBe Status.SEE_OTHER
+    response.body[String] shouldBe Constants.EMPTY_STRING
+    response.header("Location").value shouldBe AppRoutes.providedetails.MemberCheckYourAnswersController.show.url
 
   s"POST $path with agree should update the application and redirect to the task list" in:
     AuthStubs.stubAuthoriseIndividual()
