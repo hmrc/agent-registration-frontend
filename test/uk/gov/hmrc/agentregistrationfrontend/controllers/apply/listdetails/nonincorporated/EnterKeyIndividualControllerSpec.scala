3c17c534ff53912559cb18a1dd25837fcbdff737
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2026-01-30 14:02:28 +0000

    APB-10655 APB-10701 Forms and templates for individual name capture (#164)
    * APB-10655 APB-10701 forms and templates
    
    * APB-10655 fix multiple h1
    
    * APB-10655 dependencies

---
+++ test/uk/gov/hmrc/agentregistrationfrontend/controllers/apply/listdetails/nonincorporated/EnterKeyIndividualControllerSpec.scala
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentregistrationfrontend.controllers.apply.listdetails.nonincorporated
+
+import play.api.libs.ws.WSResponse
+import uk.gov.hmrc.agentregistration.shared.AgentApplicationGeneralPartnership
+import uk.gov.hmrc.agentregistrationfrontend.controllers.apply.ApplyStubHelper
+import uk.gov.hmrc.agentregistrationfrontend.forms.IndividualNameForm
+import uk.gov.hmrc.agentregistrationfrontend.testsupport.ControllerSpec
+import uk.gov.hmrc.agentregistrationfrontend.testsupport.wiremock.stubs.AgentRegistrationStubs
+
+class EnterKeyIndividualControllerSpec
+extends ControllerSpec:
+
+  private val path = "/agent-registration/apply/list-details/enter-key-individual"
+
+  object agentApplication:
+
+    val beforeHowManyKeyIndividuals: AgentApplicationGeneralPartnership =
+      tdAll
+        .agentApplicationGeneralPartnership
+        .afterHmrcStandardForAgentsAgreed
+
+    val afterHowManyKeyIndividuals: AgentApplicationGeneralPartnership =
+      tdAll
+        .agentApplicationGeneralPartnership
+        .afterHowManyKeyIndividuals
+
+    val afterOnlyOneKeyIndividual: AgentApplicationGeneralPartnership =
+      tdAll
+        .agentApplicationGeneralPartnership
+        .afterOnlyOneKeyIndividual
+
+    // SixOrMore selected and 5 or more nominated
+    val afterHowManyKeyIndividualsNeedsNoPadding: AgentApplicationGeneralPartnership =
+      tdAll
+        .agentApplicationGeneralPartnership
+        .afterHowManyKeyIndividualsNeedsNoPadding
+
+    // SixOrMore selected and less than 5 nominated
+    val afterHowManyKeyIndividualsNeedsPadding: AgentApplicationGeneralPartnership =
+      tdAll
+        .agentApplicationGeneralPartnership
+        .afterHowManyKeyIndividualsNeedsPadding
+
+  "routes should have correct paths and methods" in:
+    AppRoutes.apply.listdetails.EnterKeyIndividualController.show shouldBe Call(
+      method = "GET",
+      url = path
+    )
+    AppRoutes.apply.listdetails.EnterKeyIndividualController.submit shouldBe Call(
+      method = "POST",
+      url = path
+    )
+    AppRoutes.apply.listdetails.EnterKeyIndividualController.submit.url shouldBe AppRoutes.apply.listdetails.EnterKeyIndividualController.show.url
+
+  s"GET $path should return 200, fetch the BPR and render page for entering first partner" in:
+    ApplyStubHelper.stubsForAuthAction(agentApplication.afterHowManyKeyIndividuals)
+    AgentRegistrationStubs.stubFindIndividualsForApplication(
+      agentApplicationId = agentApplication.afterHowManyKeyIndividuals.agentApplicationId,
+      individuals = List.empty // existing list is empty and target size is 3, so this is for the "first" partner
+    )
+    val response: WSResponse = get(path)
+
+    response.status shouldBe Status.OK
+    response.parseBodyAsJsoupDocument.title() shouldBe "What is the full name of the first partner? - Apply for an agent services account - GOV.UK"
+    ApplyStubHelper.verifyConnectorsForAuthAction()
+
+  s"GET $path should return 200, fetch the BPR and render page for entering next partner" in:
+    ApplyStubHelper.stubsForAuthAction(agentApplication.afterHowManyKeyIndividuals)
+    AgentRegistrationStubs.stubFindIndividualsForApplication(
+      agentApplicationId = agentApplication.afterHowManyKeyIndividuals.agentApplicationId,
+      individuals = List(tdAll.individualProvidedDetails) // existing list already has one individual so this is the "next" partner
+    )
+    val response: WSResponse = get(path)
+
+    response.status shouldBe Status.OK
+    response.parseBodyAsJsoupDocument.title() shouldBe "What is the full name of the next partner? - Apply for an agent services account - GOV.UK"
+    ApplyStubHelper.verifyConnectorsForAuthAction()
+
+  s"GET $path should return 200, fetch the BPR and render page for entering the only partner" in:
+    ApplyStubHelper.stubsForAuthAction(agentApplication.afterOnlyOneKeyIndividual)
+    AgentRegistrationStubs.stubFindIndividualsForApplication(
+      agentApplicationId = agentApplication.afterHowManyKeyIndividuals.agentApplicationId,
+      individuals = List.empty // even though this makes this the "first" entry, there is only one required so the "only" partner heading is shown
+    )
+    val response: WSResponse = get(path)
+
+    response.status shouldBe Status.OK
+    response.parseBodyAsJsoupDocument.title() shouldBe "What is the full name of the partner? - Apply for an agent services account - GOV.UK"
+    ApplyStubHelper.verifyConnectorsForAuthAction()
+
+  s"POST $path with blank inputs should return 400" in:
+    ApplyStubHelper.stubsForAuthAction(agentApplication.afterHowManyKeyIndividuals)
+    AgentRegistrationStubs.stubFindIndividualsForApplication(
+      agentApplicationId = agentApplication.afterHowManyKeyIndividuals.agentApplicationId,
+      individuals = List.empty
+    )
+    val response: WSResponse = post(path)(Map.empty)
+
+    response.status shouldBe Status.BAD_REQUEST
+    val doc = response.parseBodyAsJsoupDocument
+    doc.title() shouldBe "Error: What is the full name of the first partner? - Apply for an agent services account - GOV.UK"
+    doc.mainContent.select(
+      s"#${IndividualNameForm.key}-error"
+    ).text() shouldBe "Error: Enter the full name of the partner"
+    ApplyStubHelper.verifyConnectorsForAuthAction()
+
+  s"POST $path with invalid inputs should return 400" in:
+    ApplyStubHelper.stubsForAuthAction(agentApplication.afterHowManyKeyIndividuals)
+    AgentRegistrationStubs.stubFindIndividualsForApplication(
+      agentApplicationId = agentApplication.afterHowManyKeyIndividuals.agentApplicationId,
+      individuals = List.empty
+    )
+    val response: WSResponse =
+      post(path)(Map(
+        IndividualNameForm.key -> Seq("Invalid@@Name123")
+      ))
+
+    response.status shouldBe Status.BAD_REQUEST
+    val doc = response.parseBodyAsJsoupDocument
+    doc.title() shouldBe "Error: What is the full name of the first partner? - Apply for an agent services account - GOV.UK"
+    doc.mainContent.select(
+      s"#${IndividualNameForm.key}-error"
+    ).text() shouldBe "Error: The partner’s name must only include letters a to z, hyphens, apostrophes and spaces"
+    ApplyStubHelper.verifyConnectorsForAuthAction()
---
+++ test/uk/gov/hmrc/agentregistrationfrontend/views/apply/listdetails/EnterIndividualNameComplexPageSpec.scala
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentregistrationfrontend.views.apply.listdetails
+
+import com.softwaremill.quicklens.modify
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.mvc.AnyContent
+import uk.gov.hmrc.agentregistration.shared.lists.SixOrMore
+import uk.gov.hmrc.agentregistrationfrontend.action.AgentApplicationRequest
+import uk.gov.hmrc.agentregistrationfrontend.forms.IndividualNameForm
+import uk.gov.hmrc.agentregistrationfrontend.model.SubmitAction.SaveAndComeBackLater
+import uk.gov.hmrc.agentregistrationfrontend.model.SubmitAction.SaveAndContinue
+import uk.gov.hmrc.agentregistrationfrontend.testsupport.ViewSpec
+import uk.gov.hmrc.agentregistrationfrontend.views.html.apply.listdetails.EnterIndividualNameComplexPage
+
+class EnterIndividualNameComplexPageSpec
+extends ViewSpec:
+
+  private val firstPartnerKey = "first"
+  private val nextPartnerKey = "subsequent"
+
+  object ExpectedStrings:
+
+    val heading = "Tell us about the partners of Test Company Name"
+    val firstPartnerLabel = "What is the full name of the first partner?"
+    val nextPartnerLabel = "What is the full name of the next partner?"
+
+  val viewTemplate: EnterIndividualNameComplexPage = app.injector.instanceOf[EnterIndividualNameComplexPage]
+  implicit val agentApplicationRequest: AgentApplicationRequest[AnyContent] = tdAll.makeAgentApplicationRequest(
+    agentApplication =
+      tdAll
+        .agentApplicationGeneralPartnership
+        .afterHowManyKeyIndividualsNeedsPadding
+  )
+
+  final case class TestCaseForComplexPage(
+    ordinalKey: String,
+    numberOfRequiredKeyIndividuals: SixOrMore,
+    expectedPaddingBullet1: String,
+    expectedPaddingBullet2: String
+  )
+
+  val ordinalKeys = List(firstPartnerKey, nextPartnerKey)
+
+  val paddingCases: List[(Int, String, String)] = List(
+    (1, "the partner responsible for tax advice", "4 other partners"),
+    (2, "the 2 partners responsible for tax advice", "3 other partners"),
+    (3, "the 3 partners responsible for tax advice", "2 other partners"),
+    (4, "the 4 partners responsible for tax advice", "1 other partner")
+  )
+
+  val testCases: List[TestCaseForComplexPage] = ordinalKeys.flatMap: ordinal =>
+    paddingCases.map:
+      case (n, bullet1, bullet2) =>
+        TestCaseForComplexPage(
+          ordinalKey = ordinal,
+          numberOfRequiredKeyIndividuals = tdAll.sixOrMoreKeyIndividuals
+            .modify(_.numberOfKeyIndividualsResponsibleForTaxMatters)
+            .setTo(n),
+          expectedPaddingBullet1 = bullet1,
+          expectedPaddingBullet2 = bullet2
+        )
+
+  testCases.foreach: testCase =>
+    val n: Int = testCase.numberOfRequiredKeyIndividuals.numberOfKeyIndividualsResponsibleForTaxMatters
+    s"EnterIndividualNameComplexPage for $n individuals using ordinal key '${testCase.ordinalKey}'" should:
+      val doc: Document = Jsoup.parse(viewTemplate(
+        form = IndividualNameForm.form,
+        ordinalKey = testCase.ordinalKey,
+        numberOfRequiredKeyIndividuals = testCase.numberOfRequiredKeyIndividuals,
+        entityName = tdAll.companyName
+      ).body)
+
+      val expectedLabel: String =
+        testCase.ordinalKey match
+          case `firstPartnerKey` => ExpectedStrings.firstPartnerLabel
+          case `nextPartnerKey` => ExpectedStrings.nextPartnerLabel
+
+      s"contain expected content for $n individuals using ordinal key '${testCase.ordinalKey}'" in:
+        doc.mainContent shouldContainContent
+          s"""
+             |Partner and tax adviser information
+             |Tell us about the partners of Test Company Name
+             |We need the names of:
+             |${testCase.expectedPaddingBullet1}
+             |${testCase.expectedPaddingBullet2}
+             |What we mean by ‘responsible for tax advice’
+             |A partner is responsible for tax advice if they have:
+             |material responsibility for tax advice activities
+             |significant authority over HMRC interactions
+             |$expectedLabel
+             |Save and continue
+             |Save and come back later
+             |Is this page not working properly? (opens in new tab)
+             |""".stripMargin
+
+      s"have the correct title for $n individuals using ordinal key '${testCase.ordinalKey}'" in:
+        doc.title() shouldBe s"${ExpectedStrings.heading} - Apply for an agent services account - GOV.UK"
+
+      s"have the correct form label for ordinal key '${testCase.ordinalKey}'" in:
+        doc
+          .mainContent
+          .selectOrFail("form label[for='individualName']")
+          .selectOnlyOneElementOrFail()
+          .text() shouldBe expectedLabel
+
+      s"render a save and continue button for $n individuals using ordinal '${testCase.ordinalKey}'" in:
+        doc
+          .mainContent
+          .selectOrFail(s"form button[value='${SaveAndContinue.toString}']")
+          .selectOnlyOneElementOrFail()
+          .text() shouldBe "Save and continue"
+
+      s"render a save and come back later button for $n individuals using ordinal '${testCase.ordinalKey}'" in:
+        doc
+          .mainContent
+          .selectOrFail(s"form button[value=${SaveAndComeBackLater.toString}]")
+          .selectOnlyOneElementOrFail()
+          .text() shouldBe "Save and come back later"
+
+      s"render a form error when the form contains an error for $n individuals using ordinal key '${testCase.ordinalKey}'" in:
+        val field = IndividualNameForm.key
+        val errorMessage = "Enter the full name of the partner"
+        val formWithError = IndividualNameForm.form
+          .withError(field, errorMessage)
+        behavesLikePageWithErrorHandling(
+          field = field,
+          errorMessage = errorMessage,
+          errorDoc = Jsoup.parse(viewTemplate(
+            form = formWithError,
+            ordinalKey = testCase.ordinalKey,
+            numberOfRequiredKeyIndividuals = testCase.numberOfRequiredKeyIndividuals,
+            entityName = tdAll.companyName
+          ).body),
+          heading = ExpectedStrings.heading
+        )
