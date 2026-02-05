6521fc5baca07486d88b80446bea1ce727e35f18
pav <173694350+paweldigital@users.noreply.github.com>
2026-02-05 16:03:16 +0000

    APB-10575 migrated IndividualTelephoneNumberController
    Signed-off-by: pav <173694350+paweldigital@users.noreply.github.com>

--- app/uk/gov/hmrc/agentregistrationfrontend/controllers/providedetails/IndividualTelephoneNumberController.scala
+++ app/uk/gov/hmrc/agentregistrationfrontend/controllers/providedetails/IndividualTelephoneNumberController.scala
@@ -17,16 +17,13 @@
 package uk.gov.hmrc.agentregistrationfrontend.controllers.providedetails
 
 import play.api.mvc.Action
-import play.api.mvc.ActionBuilder
 import play.api.mvc.AnyContent
 import play.api.mvc.MessagesControllerComponents
 import uk.gov.hmrc.agentregistration.shared.TelephoneNumber
 import uk.gov.hmrc.agentregistrationfrontend.forms.IndividualTelephoneNumberForm
 import uk.gov.hmrc.agentregistrationfrontend.views.html.providedetails.individualconfirmation.IndividualTelephoneNumberPage
 import uk.gov.hmrc.agentregistration.shared.llp.IndividualProvidedDetailsToBeDeleted
-import uk.gov.hmrc.agentregistrationfrontend.action.FormValue
 import uk.gov.hmrc.agentregistrationfrontend.action.IndividualActions
-import uk.gov.hmrc.agentregistrationfrontend.action.individual.llp.IndividualProvideDetailsRequest
 
 import com.softwaremill.quicklens.modify
 import uk.gov.hmrc.agentregistrationfrontend.services.llp.IndividualProvideDetailsService
@@ -43,8 +40,8 @@ class IndividualTelephoneNumberController @Inject() (
 )
 extends FrontendController(mcc, actions):
 
-  private val baseAction: ActionBuilder[IndividualProvideDetailsRequest, AnyContent] = actions
-    .DELETEMEgetProvideDetailsInProgress
+  private val baseAction: ActionBuilderWithData[DataWithIndividualProvidedDetails] = actions
+    .getProvideDetailsInProgress
     .ensure(
       _.individualProvidedDetails.companiesHouseMatch.nonEmpty, // TODO: Add check for companies house details
       implicit request =>
@@ -52,13 +49,13 @@ extends FrontendController(mcc, actions):
     )
 
   def submit: Action[AnyContent] = baseAction
-    .ensureValidForm[TelephoneNumber](
+    .ensureValidForm4[TelephoneNumber](
       IndividualTelephoneNumberForm.form,
       implicit r => view(_)
     )
     .async:
-      implicit request: (IndividualProvideDetailsRequest[AnyContent] & FormValue[TelephoneNumber]) =>
-        val telephoneNumberFromForm: TelephoneNumber = request.formValue
+      implicit request =>
+        val telephoneNumberFromForm: TelephoneNumber = request.get
         val updatedProvidedDetails: IndividualProvidedDetailsToBeDeleted = request
           .individualProvidedDetails
           .modify(_.telephoneNumber)
