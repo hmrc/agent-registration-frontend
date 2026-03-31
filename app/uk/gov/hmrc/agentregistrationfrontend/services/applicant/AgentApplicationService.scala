66e96c49eb7ae2ca7d90f65b61a0a86a1ebe9665
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2026-03-31 09:23:26 +0100

    APB-11012 remove comment

--- app/uk/gov/hmrc/agentregistrationfrontend/services/applicant/AgentApplicationService.scala
+++ app/uk/gov/hmrc/agentregistrationfrontend/services/applicant/AgentApplicationService.scala
@@ -45,7 +45,4 @@ extends RequestAwareLogging:
     agentApplication: AgentApplication
   )(using RequestHeader): Future[Unit] = agentRegistrationConnector.upsertApplication(agentApplication)
 
-  // we only ever delete an authorised user's own application, this will delete the application for this authorised user,
-  // so we don't need to pass in an application id or link id, the backend will find the application to delete based on
-  // the authorised user's internal id
   def deleteAndStartAgain()(using RequestHeader): Future[Unit] = agentRegistrationConnector.deleteApplication()
