17a3af5ee8abae22be6bd12d514500de083e63f2
pav <173694350+paweldigital@users.noreply.github.com>
2026-06-10 13:47:05 +0000

    [APB-11535] some comments
    Signed-off-by: pav <173694350+paweldigital@users.noreply.github.com>

--- app/uk/gov/hmrc/agentregistration/shared/ApplicationState.scala
+++ app/uk/gov/hmrc/agentregistration/shared/ApplicationState.scala
@@ -37,56 +37,65 @@ object ApplicationState:
   case object Started
   extends ApplicationState
 
-  case object GrsDataReceived
+  case object GrsDataReceived // InProgress, user performs tasks from the task list
   extends ApplicationState
 
-  case object SentForRisking
+  // set by FE when Application and Individuals were sent to agent-registration-risking
+  case object SentForRisking //
   extends ApplicationState
 
+  // set by risking when agent-registration-risking submitted Application and Individuals to minerva
   case object RiskingInProgress
   extends ApplicationState
 
+  // Set by agent-registration-risking when minerva responded with risking results and the overall risking status is known as well failures for application and individuals
   sealed trait RiskingCompleted
   extends ApplicationState:
     def riskingCompletedDate: LocalDate
 
+  // Overall status of the application is approved.
+  // The entity and all individuals passed risking screening.
   final case class Approved(override val riskingCompletedDate: LocalDate)
   extends RiskingCompleted
 
-  final case class FailedFixable(
-    fixes: Seq[EntityFix],
-    override val riskingCompletedDate: LocalDate,
-    correctiveActionExpiryDate: Option[LocalDate]
-  )
-  extends RiskingCompleted
+//Overall status of the application is failed with fixable failures.
+// Entity and individuals either passed passed risking screening or failed with fixable failures
+// if 'fixes' are empty there are no fixing tasks for this application (only for individuals)
+final case class FailedFixable(
+  fixes: Seq[EntityFix],
+  override val riskingCompletedDate: LocalDate,
+  correctiveActionExpiryDate: Option[LocalDate]
+)
+extends RiskingCompleted
 
-  final case class FailedNonFixable(
-    failures: Seq[EntityFailure],
-    override val riskingCompletedDate: LocalDate
-  )
-  extends RiskingCompleted
+//Overall status of the application is failed - this is termianal state
+final case class FailedNonFixable(
+  failures: Seq[EntityFailure], // if empty the Application is approved, but there exist individuals which caused overall status to be failed
+  override val riskingCompletedDate: LocalDate
+)
+extends RiskingCompleted
 
-  given format: OFormat[ApplicationState] =
-    given OFormat[ApplicationState.Started.type] = Json.format[ApplicationState.Started.type]
-    given OFormat[ApplicationState.GrsDataReceived.type] = Json.format[ApplicationState.GrsDataReceived.type]
-    given OFormat[ApplicationState.SentForRisking.type] = Json.format[ApplicationState.SentForRisking.type]
-    given OFormat[ApplicationState.RiskingInProgress.type] = Json.format[ApplicationState.RiskingInProgress.type]
-    given OFormat[ApplicationState.Approved] = Json.format[ApplicationState.Approved]
-    given OFormat[ApplicationState.FailedFixable] = Json.format[ApplicationState.FailedFixable]
-    given OFormat[ApplicationState.FailedNonFixable] = Json.format[ApplicationState.FailedNonFixable]
+given format: OFormat[ApplicationState] =
+  given OFormat[ApplicationState.Started.type] = Json.format[ApplicationState.Started.type]
+  given OFormat[ApplicationState.GrsDataReceived.type] = Json.format[ApplicationState.GrsDataReceived.type]
+  given OFormat[ApplicationState.SentForRisking.type] = Json.format[ApplicationState.SentForRisking.type]
+  given OFormat[ApplicationState.RiskingInProgress.type] = Json.format[ApplicationState.RiskingInProgress.type]
+  given OFormat[ApplicationState.Approved] = Json.format[ApplicationState.Approved]
+  given OFormat[ApplicationState.FailedFixable] = Json.format[ApplicationState.FailedFixable]
+  given OFormat[ApplicationState.FailedNonFixable] = Json.format[ApplicationState.FailedNonFixable]
 
-    given JsonConfiguration = JsonConfig.jsonConfiguration
+  given JsonConfiguration = JsonConfig.jsonConfiguration
 
-    val base: OFormat[ApplicationState] = Json.format[ApplicationState]
+  val base: OFormat[ApplicationState] = Json.format[ApplicationState]
 
-    val legacyStringReads: Reads[ApplicationState] = Reads {
-      case JsString("Started") => JsSuccess(Started)
-      case JsString("GrsDataReceived") => JsSuccess(GrsDataReceived)
-      case JsString("SentForRisking") => JsSuccess(SentForRisking)
-      case _ => JsError("Not a legacy string ApplicationState")
-    }
+  val legacyStringReads: Reads[ApplicationState] = Reads {
+    case JsString("Started") => JsSuccess(Started)
+    case JsString("GrsDataReceived") => JsSuccess(GrsDataReceived)
+    case JsString("SentForRisking") => JsSuccess(SentForRisking)
+    case _ => JsError("Not a legacy string ApplicationState")
+  }
 
-    OFormat(base.orElse(legacyStringReads), base)
+  OFormat(base.orElse(legacyStringReads), base)
 
-  extension (as: ApplicationState)
-    def sentForRisking: Boolean = as === ApplicationState.SentForRisking
+extension (as: ApplicationState)
+  def sentForRisking: Boolean = as === ApplicationState.SentForRisking
