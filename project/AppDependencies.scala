0af48187a90121f0dc95cf508e15f5034b05c46a
Mathew May <29102292+mghmay@users.noreply.github.com>
2026-06-11 15:59:58 +0100

    Update app dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,11 +10,11 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "13.7.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "13.8.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % hmrcMongoVersion,
     "com.softwaremill.quicklens" %% "quicklens" % "1.9.12", // Updated for Scala 3 compatibility
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.5.0",
-    "uk.gov.hmrc.objectstore" %% "object-store-client-play-30" % "2.5.0",
+    "uk.gov.hmrc.objectstore" %% "object-store-client-play-30" % "2.6.0",
     "uk.gov.hmrc" %% "crypto-json-play-30" % "8.4.0",
     "uk.gov.hmrc" %% "domain-test-play-30" % "13.0.0" // Needed in this scope for UTR generation in our test-only GRS stub
   )
