a21eb93d4cda7bddc97b01edb1fb1072d61098ea
pav <173694350+paweldigital@users.noreply.github.com>
2026-06-19 13:33:54 +0000

    [APB-11490] td scenario wip
    Signed-off-by: pav <173694350+paweldigital@users.noreply.github.com>

--- app/uk/gov/hmrc/agentregistration/shared/testdata/TdScenario.scala
+++ app/uk/gov/hmrc/agentregistration/shared/testdata/TdScenario.scala
@@ -18,6 +18,7 @@ package uk.gov.hmrc.agentregistration.shared.testdata
 
 import uk.gov.hmrc.agentregistration.shared.AgentApplication
 import uk.gov.hmrc.agentregistration.shared.individual.IndividualProvidedDetails
+import uk.gov.hmrc.agentregistration.shared.testdata.LlpScenarios.AfterConfirmTwoChOfficers.tdApplicationFixture
 
 trait TdScenario:
 
@@ -43,4 +44,156 @@ object LlpScenarios:
     override val individual1: Option[IndividualProvidedDetails] = None
     override val individual2: Option[IndividualProvidedDetails] = None
 
-object AfterRefusalToDealWithCheckPass extends ...
+  object AfterRefusalToDealWithCheckPass
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterRefusalToDealWithCheckPass
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterRefusalToDealWithCheckFail
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterRefusalToDealWithCheckFail
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterUnifiedCustomerRegistryUpdateIdentifiers
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterUnifiedCustomerRegistryUpdateIdentifiers
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterUnifiedCustomerRegistryUpdateEmptyIdentifiers
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterUnifiedCustomerRegistryUpdateEmptyIdentifiers
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterGlobalAsaEnrolmentCheckPass
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterGlobalAsaEnrolmentCheckPass
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterGlobalAsaEnrolmentCheckFail
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterGlobalAsaEnrolmentCheckFail
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterContactDetailsComplete
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterContactDetailsComplete
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterAgentDetailsComplete
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterAgentDetailsComplete
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterAmlsComplete
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterAmlsComplete
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterHmrcStandardForAgentsAgreed
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterHmrcStandardForAgentsAgreed
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterZeroCompaniesHouseOfficers
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterZeroCompaniesHouseOfficers
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterConfirmCompaniesHouseOfficersYes
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterConfirmCompaniesHouseOfficersYes
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterNumberOfConfirmCompaniesHouseOfficers
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterNumberOfConfirmCompaniesHouseOfficers
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterConfirmTwoChOfficers
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterConfirmTwoChOfficers
+
+    override val individual1: Option[IndividualProvidedDetails] = Some(tdApplicationFixture.tdIndividualsInStates.invididualInStates1.precreated)
+    override val individual2: Option[IndividualProvidedDetails] = Some(tdApplicationFixture.tdIndividualsInStates.invididualInStates2.precreated)
+
+  object AfterConfirmSixChOfficers
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterConfirmSixChOfficers
+
+    override val individual1: Option[IndividualProvidedDetails] = Some(tdApplicationFixture.tdIndividualsInStates.invididualInStates1.precreated)
+    override val individual2: Option[IndividualProvidedDetails] = Some(tdApplicationFixture.tdIndividualsInStates.invididualInStates2.precreated)
+
+  object AfterConfirmCompaniesHouseOfficersNo
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterConfirmCompaniesHouseOfficersNo
+    override val individual1: Option[IndividualProvidedDetails] = None
+    override val individual2: Option[IndividualProvidedDetails] = None
+
+  object AfterConfirmOtherRelevantTaxAdvisersNo
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterConfirmOtherRelevantTaxAdvisersNo
+    override val individual1: Option[IndividualProvidedDetails] = Some(tdApplicationFixture.tdIndividualsInStates.invididualInStates1.precreated)
+    override val individual2: Option[IndividualProvidedDetails] = Some(tdApplicationFixture.tdIndividualsInStates.invididualInStates2.precreated)
+
+
+  object BeforeDeclarationSubmitted:
+    def (i1Status, i2Status)
+    object I1PrecreatedI2Precreated extends TdScenario
+    object I1afterAccessConfirmedI2Precreated extends TdScenario
+    object afterStartedConfirmedI2Precreated extends TdScenario
+    object afterTelephoneNumberProvidedConfirmedI2Precreated extends TdScenario
+
+  object AfterDeclarationSubmitted
+  extends TdScenario:
+
+    val tdApplicationFixture: TdApplicationFixture = TdApplicationFixture.make(this.toString)
+    override val agentApplication: AgentApplication = tdApplicationFixture.applicationLlpInStates.afterDeclarationSubmitted
+    override val individual1: Option[IndividualProvidedDetails] = Some(tdApplicationFixture.tdIndividualsInStates.invididualInStates1.afterFinished)
+    override val individual2: Option[IndividualProvidedDetails] = Some(tdApplicationFixture.tdIndividualsInStates.invididualInStates2.afterFinished)
