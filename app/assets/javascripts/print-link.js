601b74e21726421cc80b4b519b5f87aeea76c5fe
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2026-07-15 09:39:32 +0100

    APB-11196 use js to show print link

--- app/assets/javascripts/print-link.js
+++ app/assets/javascripts/print-link.js
@@ -1,4 +1,8 @@
 (function (window, document) {
+    document.querySelectorAll('.not-supported')
+        .forEach(function (element) {
+            element.classList.remove('not-supported')
+        })
     document.querySelectorAll('a[href="#print-dialogue"]')
         .forEach(function(link) {
             link.addEventListener('click', function(event) {
