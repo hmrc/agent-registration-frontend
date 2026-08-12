86f94694f24f697067b4d421399ec67fd4804397
wojciech.gradzki <gitwojciech@users.noreply.github.com>
2026-08-12 15:27:47 +0100

    [APB-11886] data integrity checks, PR comments
    Signed-off-by: wojciech.gradzki <gitwojciech@users.noreply.github.com>

--- app/uk/gov/hmrc/agentregistration/shared/dataintegrity/DataIntegrity.scala
+++ app/uk/gov/hmrc/agentregistration/shared/dataintegrity/DataIntegrity.scala
@@ -34,82 +34,32 @@ object DataIntegrity:
   private def requireThat(
     condition: Boolean,
     message: => String
-  ): Seq[String] =
-    if !condition
-    then Seq(message)
-    else Seq.empty
+  ): Option[String] = if !condition then Some(message) else None
 
   private def violationsBeforeSubmission(agentApplication: AgentApplication)(using state: ApplicationState): Seq[String] =
-    requireThat(
-      agentApplication.applicationExpiresAt.isDefined,
-      s"applicationExpiresAt should be defined in $state state"
-    ) ++
-      requireThat(
-        agentApplication.submittedAt.isEmpty,
-        s"submittedAt should not be defined in $state state"
-      ) ++
-      requireThat(
-        agentApplication.riskingOutcomeApplication.isEmpty,
-        s"riskingOutcomeApplication should not be defined in $state state"
-      ) ++
-      requireThat(
-        agentApplication.riskingOutcomeEntity.isEmpty,
-        s"riskingOutcomeEntity should not be defined in $state state"
-      )
+    Seq(
+      requireThat(agentApplication.applicationExpiresAt.isDefined, s"applicationExpiresAt should be defined in $state state"),
+      requireThat(agentApplication.submittedAt.isEmpty, s"submittedAt should not be defined in $state state"),
+      requireThat(agentApplication.riskingOutcomeApplication.isEmpty, s"riskingOutcomeApplication should not be defined in $state state"),
+      requireThat(agentApplication.riskingOutcomeEntity.isEmpty, s"riskingOutcomeEntity should not be defined in $state state")
+    ).flatten
 
   private def violationsAfterSubmission(agentApplication: AgentApplication)(using state: ApplicationState): Seq[String] =
-    requireThat(
-      agentApplication.applicationExpiresAt.isEmpty,
-      s"applicationExpiresAt should not be defined in $state state"
-    ) ++
-      requireThat(
-        agentApplication.submittedAt.isDefined,
-        s"submittedAt should be defined in $state state"
-      ) ++
-      requireThat(
-        businessDetailsDefined(agentApplication),
-        s"businessDetails should be defined in $state state"
-      ) ++
-      requireThat(
-        agentApplication.applicantContactDetails.isDefined,
-        s"applicantContactDetails should be defined in $state state"
-      ) ++
-      requireThat(
-        agentApplication.applicantContactDetails.forall(_.isComplete),
-        s"applicantContactDetails should be complete in $state state"
-      ) ++
-      requireThat(
-        agentApplication.amlsDetails.isDefined,
-        s"amlsDetails should be defined in $state state"
-      ) ++
-      requireThat(
-        agentApplication.amlsDetails.forall(_.isComplete),
-        s"amlsDetails should be complete in $state state"
-      ) ++
-      requireThat(
-        agentApplication.agentDetails.isDefined,
-        s"agentDetails should be defined in $state state"
-      ) ++
-      requireThat(
-        agentApplication.agentDetails.forall(_.isComplete),
-        s"agentDetails should be complete in $state state"
-      ) ++
-      requireThat(
-        agentApplication.refusalToDealWithCheckResult.isDefined,
-        s"refusalToDealWithCheckResult should be defined in $state state"
-      ) ++
-      requireThat(
-        agentApplication.globalAsaEnrolmentCheckResult.isDefined,
-        s"globalAsaEnrolmentCheckResult should be defined in $state state"
-      ) ++
-      requireThat(
-        agentApplication.vrns.isDefined,
-        s"vrns should be defined in $state state"
-      ) ++
-      requireThat(
-        agentApplication.payeRefs.isDefined,
-        s"payeRefs should be defined in $state state"
-      )
+    Seq(
+      requireThat(agentApplication.applicationExpiresAt.isEmpty, s"applicationExpiresAt should not be defined in $state state"),
+      requireThat(agentApplication.submittedAt.isDefined, s"submittedAt should be defined in $state state"),
+      requireThat(businessDetailsDefined(agentApplication), s"businessDetails should be defined in $state state"),
+      requireThat(agentApplication.applicantContactDetails.isDefined, s"applicantContactDetails should be defined in $state state"),
+      requireThat(agentApplication.applicantContactDetails.forall(_.isComplete), s"applicantContactDetails should be complete in $state state"),
+      requireThat(agentApplication.amlsDetails.isDefined, s"amlsDetails should be defined in $state state"),
+      requireThat(agentApplication.amlsDetails.forall(_.isComplete), s"amlsDetails should be complete in $state state"),
+      requireThat(agentApplication.agentDetails.isDefined, s"agentDetails should be defined in $state state"),
+      requireThat(agentApplication.agentDetails.forall(_.isComplete), s"agentDetails should be complete in $state state"),
+      requireThat(agentApplication.refusalToDealWithCheckResult.isDefined, s"refusalToDealWithCheckResult should be defined in $state state"),
+      requireThat(agentApplication.globalAsaEnrolmentCheckResult.isDefined, s"globalAsaEnrolmentCheckResult should be defined in $state state"),
+      requireThat(agentApplication.vrns.isDefined, s"vrns should be defined in $state state"),
+      requireThat(agentApplication.payeRefs.isDefined, s"payeRefs should be defined in $state state")
+    ).flatten
 
   private object FirstSubmission:
 
@@ -127,22 +77,18 @@ object DataIntegrity:
     private def whenGrsDataReceived(agentApplication: AgentApplication): Seq[String] =
       given state: ApplicationState = ApplicationState.GrsDataReceived
       violationsBeforeSubmission(agentApplication) ++
-        requireThat(
-          businessDetailsDefined(agentApplication),
-          s"businessDetails should be defined in $state state"
-        )
+        requireThat(businessDetailsDefined(agentApplication), s"businessDetails should be defined in $state state")
 
     private def whenAwaitingRiskingOutcome(agentApplication: AgentApplication): Seq[String] =
       given state: ApplicationState = agentApplication.applicationState
       violationsAfterSubmission(agentApplication) ++
-        requireThat(
-          agentApplication.riskingOutcomeApplication.isEmpty,
-          s"riskingOutcomeApplication should not be defined in $state state during first submission"
-        ) ++
-        requireThat(
-          agentApplication.riskingOutcomeEntity.isEmpty,
-          s"riskingOutcomeEntity should not be defined in $state state during first submission"
-        )
+        Seq(
+          requireThat(
+            agentApplication.riskingOutcomeApplication.isEmpty,
+            s"riskingOutcomeApplication should not be defined in $state state during first submission"
+          ),
+          requireThat(agentApplication.riskingOutcomeEntity.isEmpty, s"riskingOutcomeEntity should not be defined in $state state during first submission")
+        ).flatten
 
   private object Resubmission:
 
@@ -150,32 +96,21 @@ object DataIntegrity:
       agentApplication.applicationState match
         case ApplicationState.SentForRisking | ApplicationState.SentToMinerva => whenAwaitingRiskingOutcome(agentApplication)
         case ApplicationState.RiskingCompleted => whenRiskingCompleted(agentApplication)
-        case other =>
-          requireThat(
-            false,
-            s"resubmission-in-flight is only valid in SentForRisking, SentToMinerva or RiskingCompleted, found $other"
-          )
+        case other => Seq(s"resubmission-in-flight is only valid in SentForRisking, SentToMinerva or RiskingCompleted, found $other")
 
     private def whenAwaitingRiskingOutcome(agentApplication: AgentApplication): Seq[String] =
       given state: ApplicationState = agentApplication.applicationState
       // riskingOutcomeApplication is already guaranteed by the dispatch in `violations`, so only riskingOutcomeEntity is asserted here.
       violationsAfterSubmission(agentApplication) ++
-        requireThat(
-          agentApplication.riskingOutcomeEntity.isDefined,
-          s"riskingOutcomeEntity should be preserved in $state state during resubmission"
-        )
+        requireThat(agentApplication.riskingOutcomeEntity.isDefined, s"riskingOutcomeEntity should be preserved in $state state during resubmission")
 
   private def whenRiskingCompleted(agentApplication: AgentApplication): Seq[String] =
     given ApplicationState = ApplicationState.RiskingCompleted
     violationsAfterSubmission(agentApplication) ++
-      requireThat(
-        agentApplication.riskingOutcomeApplication.isDefined,
-        "riskingOutcomeApplication should be defined in RiskingCompleted state"
-      ) ++
-      requireThat(
-        agentApplication.riskingOutcomeEntity.isDefined,
-        "riskingOutcomeEntity should be defined in RiskingCompleted state"
-      )
+      Seq(
+        requireThat(agentApplication.riskingOutcomeApplication.isDefined, "riskingOutcomeApplication should be defined in RiskingCompleted state"),
+        requireThat(agentApplication.riskingOutcomeEntity.isDefined, "riskingOutcomeEntity should be defined in RiskingCompleted state")
+      ).flatten
 
   private def businessDetailsDefined(agentApplication: AgentApplication): Boolean =
     agentApplication match
